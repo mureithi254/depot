b8fa6e3cdb8c6a611f40069cce14f1554998eecd
mureithi254 <lnewhenio@gmail.com>
2017-01-03 12:53:02 +0300

    "correct layouts added this time"

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,52 +3,57 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
-    @update = { 
-      title: 'Lorem Ipsum',
-      description: 'Best book for web developers',
-      image_url: '/images/pic3.jpg',
-      price: 13.00
+    @update = {
+     title:'Lorem Ipsum',
+     description: 'Wibbles are fun!',
+     image_url:'lorem.jpg',
+     price:19.95
     }
   end
 
   test "should get index" do
-    get products_url
-    assert_response :success
+   get products_url
+   assert_response :success
   end
 
   test "should get new" do
-    get new_product_url
-    assert_response :success
+   get new_product_url
+   assert_response :success
   end
 
   test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
-    post products_url, params: { product: @update }      
+   assert_difference('Product.count') do
+   post products_url, params: { product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
   end
 
   test "should show product" do
-    get product_url(@product)
-    assert_response :success
+   get product_url(@product)
+   assert_response :success
   end
 
   test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
+   get edit_product_url(@product)
+   assert_response :success
   end
 
   test "should update product" do
-    patch products_url(@update), params: {product: @update}
-    patch product_url(@product), params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
-    assert_redirected_to product_url(@product)
+   patch product_url(@product), params: { product: @update }
+   assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+   assert_difference('Product.count', 0) do
+   delete product_url(products(:two))
+  end
+   assert_redirected_to products_url
   end
 
   test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
+   assert_difference('Product.count', -1) do
+   delete product_url(@product)
     end
 
     assert_redirected_to products_url
